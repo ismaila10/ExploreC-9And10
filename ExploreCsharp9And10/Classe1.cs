cd3564b99f840363a2359c1d84f4c72cc16da403
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 15:38:13 +0100

    Record problematique

--- ExploreCsharp9And10/Classe1.cs
+++ ExploreCsharp9And10/Classe1.cs
@@ -8,19 +8,22 @@ namespace ExploreCsharp9And10
 {
     public class Classe1
     {
-        public int Nombre { get; init; }
+        public string Data { get; init; }
+        public int Value { get; init; }
+        public DateTime Date { get; init; }
+
+        // Pour avoir un true sur la comparaison ds la méthode myMethod je dois implémenter l'override et mettre l'algo correspondant ce qui n'est pas top
+        public override bool Equals(object? obj) => Equals(obj as Classe1);
     }
 
     class Class2
     {
         void myMethod()
         {
-            // init permet de créer une propriété qui a le mm comportement que le readonly sauf que pas obligé de créer un
-            // constructeur pour passer la valeur constructeur
-            var c = new Classe1 { Nombre = 10 };
-
-            // parcontre on ne peut pas affecter de valeur à la propriété après l'initialisation
-            c.Nombre = 10;
+            var c = new Classe1 { Data = "toto", Value = 42, Date = DateTime.UtcNow };
+            var c2 = new Classe1 { Data = "toto", Value = 42, Date = DateTime.UtcNow };
+            // sont identiques sur leurs contenus mais la référence en mémoire n'est pas la mm
+            c.Equals(c2);  //false
         }
     }
 }
