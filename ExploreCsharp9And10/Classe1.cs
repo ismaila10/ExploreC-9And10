a5c485ce0ec042cff8aa2258da2570576637eb45
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 15:05:22 +0100

    Pourquoi la nouveaute init => Cas d'erreur 1

---
+++ ExploreCsharp9And10/Classe1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreCsharp9And10
+{
+    public class Classe1
+    {
+        public int Nombre { get; }
+
+        public Classe1(int nombre)
+        {
+            Nombre = nombre;
+        }
+    }
+
+    class Class2
+    {
+        void myMethod()
+        {
+            // Avant impossible d'utiliser un initialiseur sans mettre un setter en public et enlever le constructeur au niveau de Class1
+            var c = new Classe1 { Nombre = 10 };
+        }
+    }
+}
