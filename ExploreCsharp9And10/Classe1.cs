3423f33e2edf86ed57cb43b1d2f9ed88c592a869
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 15:57:15 +0100

    Utilisation Record => raccourcis 1

--- ExploreCsharp9And10/Classe1.cs
+++ ExploreCsharp9And10/Classe1.cs
@@ -6,24 +6,22 @@ using System.Threading.Tasks;
 
 namespace ExploreCsharp9And10
 {
-    public class Classe1
+    public record Classe1
     {
         public string Data { get; init; }
         public int Value { get; init; }
         public DateTime Date { get; init; }
-
-        // Pour avoir un true sur la comparaison ds la méthode myMethod je dois implémenter l'override et mettre l'algo correspondant ce qui n'est pas top
-        public override bool Equals(object? obj) => Equals(obj as Classe1);
     }
 
-    class Class2
+    record Class2
     {
         void myMethod()
         {
             var c = new Classe1 { Data = "toto", Value = 42, Date = DateTime.UtcNow };
             var c2 = new Classe1 { Data = "toto", Value = 42, Date = DateTime.UtcNow };
-            // sont identiques sur leurs contenus mais la référence en mémoire n'est pas la mm
-            c.Equals(c2);  //false
+            // Class => myClass1.Equals(Class2) le Runtime va utiliser la methode ReferenceEquals() entre 2 objets entre autres ragarder si la référence memoire c les mm
+            // Record => record.Equals(record2) le Runtime prend toutes tes données passées et les comparer
+            c.Equals(c2);  // true
         }
     }
 }
