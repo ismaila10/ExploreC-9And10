990f7839de10ed5f389153e336672bb0a4b249ce
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 17:06:41 +0100

    Top level statement => ok 2

--- ExploreCsharp9And10/Program.cs
+++ ExploreCsharp9And10/Program.cs
@@ -1,8 +1,14 @@
 ﻿using ExploreCsharp9And10;
 Console.WriteLine("Hello, World!");
 
-// le mot clé private est souligné pck je suis toujours dans le cas de ma méthode Main 
-private void myMethode()
+// on peut néanmoins créer une méthode locale (possible depuis la verson 7 de C#)
+void myMethode()
 {
+    Console.WriteLine("Top");
+}
 
+// on peut également créer une méthode static locale (possible depuis la verson 8 de C#)
+static int myMethode2()
+{
+    return 1;
 }
