c9557240097dad6bd508cb20ee57954efa4669b8
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 18:12:55 +0100

    Pattern matching => Utilisation

--- ExploreCsharp9And10/Classe1.cs
+++ ExploreCsharp9And10/Classe1.cs
@@ -8,16 +8,31 @@ namespace ExploreCsharp9And10
 {
     public record Classe1
     {
-        public void mymethode()
+        public void myMethode()
         {
             object a = null;
-            //C# offre la possbilité de redéfinir les opèrateur, c à d que le '==' sur un objet que tu reçois en params c pas sûr que tu vas appeler ReferenceEquals(par défaut)
-            // tu t'exposes à un risque de dire o lieu de tester vraiment si mon objet pointe sur du néant , d'utiliser un opèrateur qui a été redéfini
-            if (a == null)
+            // Utilisation du pattern matching
+            if (a is null)
             {
 
             }
+            if(a is not null) // avant (!(a is null))
+            {
+
+            }
+
+            decimal d = 20;
+
+            var x = d switch
+            {
+                <10 => "inférieur à 10",
+                >= 11 and < 20 => "",
+                _ => "defaut"
+            };
+
         }
+
+
     }
 }
 
