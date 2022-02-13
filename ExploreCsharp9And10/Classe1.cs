367c8a77f47097fa056de518cc01f0c66441cc1a
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 22:55:08 +0100

    deconstructeur exemple

--- ExploreCsharp9And10/Classe1.cs
+++ ExploreCsharp9And10/Classe1.cs
@@ -1,40 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ExploreCsharp9And10;
 
-namespace ExploreCsharp9And10
+public record Classe1
 {
-    public record Classe1
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public void myMethode()
-        {
-            object a = null;
-            // Utilisation du pattern matching
-            if (a is null)
-            {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateTime DateCreation { get; set; }
 
-            }
-            if(a is not null) // avant (!(a is null))
-            {
+    public Classe1(string name, int age, DateTime dateCreation) => (Name, Age, DateCreation) = (name, age, dateCreation);
 
-            }
-
-            decimal d = 20;
-
-            var x = d switch
-            {
-                <10 => "inférieur à 10",
-                >= 11 and < 20 => "",
-                _ => "defaut"
-            };
-
-        }
-
-
-    }
+    public void Deconstruct(out string name, out int age, out DateTime dateCreation) => (name, age, dateCreation) = (Name, Age, DateCreation);
 }
 
--- ExploreCsharp9And10/Program.cs
+++ ExploreCsharp9And10/Program.cs
@@ -1,7 +1,7 @@
 ﻿using ExploreCsharp9And10;
 
-// vous pouvez omettre le type dans une new expression lorsque le type de l’objet créé est déjà connu. L’utilisation la plus courante est dans les déclarations de champ :
-List<Classe1> classes = new();
-Classe1 classes1 = new() { Name = "toto", Age = 10 };
-Dictionary<string, object> _properties = new();
+Classe1 cl = new("toto", 15, DateTime.UtcNow);
 
+var (name, age, dateCreation) = cl;
+
+Console.WriteLine($"{name} a {age} ans, il est né le {dateCreation} ");
