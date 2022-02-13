245b2497cf15c44d0bdff3a44f19824f491870ba
ismaila10 <lodia.ismaila@gmail.com>
2022-02-13 16:07:04 +0100

    Record => With Expression raccourcis 2

--- ExploreCsharp9And10/Classe1.cs
+++ ExploreCsharp9And10/Classe1.cs
@@ -18,10 +18,11 @@ namespace ExploreCsharp9And10
         void myMethod()
         {
             var c = new Classe1 { Data = "toto", Value = 42, Date = DateTime.UtcNow };
-            var c2 = new Classe1 { Data = "toto", Value = 42, Date = DateTime.UtcNow };
-            // Class => myClass1.Equals(Class2) le Runtime va utiliser la methode ReferenceEquals() entre 2 objets entre autres ragarder si la référence memoire c les mm
-            // Record => record.Equals(record2) le Runtime prend toutes tes données passées et les comparer
-            c.Equals(c2);  // true
+
+            // with expression crée une nouvelle instance d’enregistrement qui est une copie d’une instance d’enregistrement existante, avec les propriétés et les champs spécifiés modifiés.
+            var c2 = c with { Value = 47 };
+
+            c.Equals(c2);  // false
         }
     }
 }
