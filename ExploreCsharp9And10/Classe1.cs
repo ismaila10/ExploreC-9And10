de21539165e7d1716d383106e425495a6543f58f
ismaila10 <lodia.ismaila@gmail.com>
2022-02-14 12:53:39 +0100

    OK

--- ExploreCsharp9And10/Classe1.cs
+++ ExploreCsharp9And10/Classe1.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ExploreCsharp9And10;
 
-namespace ExploreCsharp9And10
+internal struct Adresse
 {
-    internal struct Adresse
+    public string Rue { get; init; } = "";
+    public string CodePostal { get; init; } = "";
+
+    public Adresse(string rue, string codePostal)
     {
-        public string Rue { get; init; } = "";
-        public string CodePostal { get; init; } = "";
+        Rue = rue;
+        CodePostal = codePostal;
+    }
 
-        public Adresse(string rue, string codePostal)
-        {
-            Rue = rue;
-            CodePostal = codePostal;
-        }
-
-        // Possible depuis C#10 à condition d'initialiser les propriétés soit à la définition(cme c le cas ici) soit ds le constructeur par défaut ci dessous
-        public Adresse()
-        {
-        }
+    // Possible depuis C#10 à condition d'initialiser les propriétés soit à la définition(cme c le cas ici) soit ds
+    // le constructeur par défaut ci dessous
+    public Adresse()
+    {
     }
 }
 
